15ff8c05018441acaec32e7a9da3b37c5b549b9f
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2021-02-23 00:39:58 +0000

    home css and events

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ import { LineGeometry } from "https://threejs.org/examples/jsm/lines/LineGeometr
 import { LineMaterial } from "https://threejs.org/examples/jsm/lines/LineMaterial.js";
 import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 import { Line2 } from "https://threejs.org/examples/jsm/lines/Line2.js";
-import { GeometryUtils } from "https://threejs.org/examples/jsm/utils/GeometryUtils.js";
 
 import { mapValue, restrain, getMs, getMousePosElem } from "./helpers.js";
 import { Vector } from "./classes.js";
@@ -12,15 +11,22 @@ window.addEventListener(
   "load",
   function main(evt) {
     window.removeEventListener(evt.type, main, false);
+
     const canvasDiv = document.getElementById("canvasDiv");
-    /*     const optionsDiv = document.getElementById("optionsDiv");
-    const gravityRange = document.getElementById("rangeInput");
+    const switchInput = document.getElementById("switchInput");
     const resetButton = document.getElementById("resetButton");
-    const switchInput = document.getElementById("switchInput"); */
+
+    switchInput.addEventListener("change", (evt) => {
+      controls.autoRotate = switchInput.checked;
+    });
+    resetButton.addEventListener("click", (evt) => {
+      console.log( camera.position)
+      controls.reset();
+    });
 
     const width = window.screen.height / 1.8;
     const height = window.screen.height / 1.8;
-    let renderer, scene, camera, controls, matLine;
+    let renderer, scene, camera, matLine, controls;
     const positions = [];
     const colors = [];
     const MAX_POINTS = 1000;
@@ -41,24 +47,22 @@ window.addEventListener(
       canvasDiv.appendChild(renderer.domElement);
       scene = new THREE.Scene();
       camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-      camera.position.set(125, 0, 0);
+      camera.position.set(-140,60,0);
+
       const point = new THREE.Vector3();
       const color = new THREE.Color();
-      for (let i = 0; i < MAX_POINTS; i++) {
-        const t = i / MAX_POINTS;
+      for (let i = 0, n = 0; i < MAX_POINTS; i++) {
         const dx = a * (y - x);
         const dy = x * (b - z) - y;
         const dz = x * y - c * z;
         x += dx * dt;
         y += dy * dt;
         z += dz * dt;
+        if (n > 1) n = 0;
         positions.push(x, y, z);
-        color.setHSL(t, 1.0, 0.5);
+        color.setHSL((n += 0.005), 1.0, 0.5);
         colors.push(color.r, color.g, color.b);
       }
-      controls = new OrbitControls(camera, renderer.domElement);
-      controls.target = new THREE.Vector3(x, y, z);
-      controls.update()
 
       const geometry = new LineGeometry();
       geometry.setPositions(positions);
@@ -68,13 +72,18 @@ window.addEventListener(
         linewidth: 3,
         vertexColors: true,
       });
-
+      controls = new OrbitControls(camera, renderer.domElement);
+      controls.target = new THREE.Vector3(x, y, z);
+      controls.minDistance = 10;
+      controls.maxDistance = 500;
+      controls.saveState()
       scene.add(new Line2(geometry, matLine));
       renderer.render(scene, camera);
     }
 
     function animate() {
       requestAnimationFrame(animate);
+      controls.update();
       renderer.setClearColor(0x000000, 0);
       renderer.setViewport(0, 0, width, height);
       matLine.resolution.set(width, height);
